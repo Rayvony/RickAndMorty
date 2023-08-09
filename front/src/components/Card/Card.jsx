a7f1c4d7884811f4d0e2ba091720d094697a518d
Mariano Vargas <marianoluisvargas@gmail.com>
2023-08-08 22:00:41 -0300

    conexion con la api, solucionado el css, completar la barra de busqueda

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-const Card = ({ characterUrl }) => {
-  const [characterData, setCharacterData] = useState(null);
-
-  useEffect(() => {
-    fetch(characterUrl)
-      .then(response => response.json())
-      .then(data => setCharacterData(data))
-      .catch(error => console.error('Error fetching character data:', error));
-  }, [characterUrl]);
-
-  if (!characterData) {
-    return <div>Loading...</div>;
-  }
-
-  const { name, status, species, type, gender, origin, location, image, episode } = characterData;
-
+export default function Card(props){
   return (
     <div className="Card">
-      <img src={image} alt={name} />
-      <h2>{name}</h2>
-      <p>Status: {status}</p>
-      <p>Species: {species}</p>
-      <p>Type: {type}</p>
-      <p>Gender: {gender}</p>
-      <p>Origin: {origin.name}</p>
-      <p>Location: {location.name}</p>
-      <p>First appearance: episode {episode[0]}</p>
+      <img src={props.image} alt={props.name} />
+      <h2>{props.name}</h2>
+      <p>Status: {props.status}</p>
+      <p>Species: {props.species}</p>
+      <p>Type: {props.type}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Origin: {props.origin.name}</p>
+      <p>Location: {props.location.name}</p>
+      <p>First appearance: episode {props.episode[0]}</p>
     </div>
   );
-};
-
-export default Card;
+}
--- front/src/components/Cards/Cards.jsx
+++ front/src/components/Cards/Cards.jsx
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Card from '../Card/Card';
 
 const Cards = () => {
-    const characterUrls = [
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/1?key=henrym-rayvony',
-        'https://rym2-production.up.railway.app/api/character/2?key=henrym-rayvony'
-      ]
-      
-    return(
-    <div className="containerCards"> 
-      {characterUrls.map((url, index) => (
-        <Card key={index} characterUrl={url} />
+  const [characterDataArray, setCharacterDataArray] = useState([]);
+
+  useEffect(() => {
+    const apiKey = 'henrym-rayvony'; // Reemplaza con tu API key
+    const apiUrl = 'https://rym2-production.up.railway.app/api/character/';
+
+    const fetchAllCharacterData = async () => {
+      try {
+        const response = await fetch(apiUrl + `?key=${apiKey}`);
+        const data = await response.json();
+        
+        // Obtener los IDs de personajes de los datos
+        const characterIds = data.results.map(character => character.id);
+
+        // Realizar solicitudes de manera paralela
+        const fetchPromises = characterIds.map(async (characterId) => {
+          const response = await fetch(`${apiUrl}${characterId}?key=${apiKey}`);
+          const characterData = await response.json();
+          return characterData;
+        });
+
+        const charactersData = await Promise.all(fetchPromises);
+        setCharacterDataArray(charactersData);
+      } catch (error) {
+        console.error('Error fetching character data:', error);
+      }
+    };
+
+    fetchAllCharacterData();
+  }, []);
+
+  return (
+    <div className="containerCards">
+      {characterDataArray.map((characterData, index) => (
+        <Card
+          key={index}
+          name={characterData.name}
+          status={characterData.status}
+          species={characterData.species}
+          type={characterData.type}
+          gender={characterData.gender}
+          origin={characterData.origin}
+          location={characterData.location}
+          image={characterData.image}
+          episode={characterData.episode}
+        />
       ))}
     </div>
   );
-}
+};
 
 export default Cards;
